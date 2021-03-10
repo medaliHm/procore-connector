d3bf9653925c50fcd0973f8fbd85addf0da55eed
medaliHm <ehm.medali@gmail.com>
2021-03-10 19:06:21 +0100

    Am

--- src/main/java/com/procore/connector/TestController.java
+++ src/main/java/com/procore/connector/TestController.java
@@ -1,12 +1,15 @@
 package com.procore.connector;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,19 +36,49 @@ public class TestController {
 
 	}
 	
+	
+	public  void runPower() throws IOException {
+		
+		  //Getting the version
+		  String command = "powershell.exe  \""+ResourceUtils.getFile(
+			      "classpath:mount.ps").getAbsolutePath()+"\"";
+		  // Executing the command
+		  Process powerShellProcess = Runtime.getRuntime().exec(command);
+		  // Getting the results
+		  powerShellProcess.getOutputStream().close();
+		  String line;
+		  System.err.println("Standard Output:");
+		  BufferedReader stdout = new BufferedReader(new InputStreamReader(
+		    powerShellProcess.getInputStream()));
+		  while ((line = stdout.readLine()) != null) {
+		   System.err.println(line);
+		  }
+		  stdout.close();
+		  System.err.println("Standard Error:");
+		  BufferedReader stderr = new BufferedReader(new InputStreamReader(
+		    powerShellProcess.getErrorStream()));
+		  while ((line = stderr.readLine()) != null) {
+		   System.err.println(line);
+		  }
+		  stderr.close();
+		  System.err.println("Done");
+
+	}
 	@PostMapping("/try")
 	public ResponseEntity<String> test(@RequestBody Object body) throws IOException {
-		File f = new File(shareDrive+"/test");
-f.createNewFile();
+		runPower();
+		File f = new File("Z:\\");
 		System.err.println(f.exists());
-		System.out.println(body.toString());
+		System.err.println(body.toString());
 		return new ResponseEntity<String>(body.toString(), HttpStatus.OK);
 
 	}
 	@PostMapping("/tryit")
 	public ResponseEntity<String> test2() throws IOException {
-		System.out.println("test");
+		System.err.println("test");
 		return new ResponseEntity<String>("test", HttpStatus.OK);
 
 	}
+	
+	
 }
