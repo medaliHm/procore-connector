0b7901a2e0a3fff98f003d6ccc8e8ad1628b186d
medaliHm <ehm.medali@gmail.com>
2021-03-10 17:16:44 +0100

    Add Sync Commit5

--- src/main/java/com/procore/connector/service/SyncService.java
+++ src/main/java/com/procore/connector/service/SyncService.java
@@ -28,58 +28,63 @@ public class SyncService {
 	private RootFolderRepository folderRepository;
 
 	public void processEvent(Event body) throws IOException {
-		if (body.getResource_name().contains("Files")) {
-			Files webhookFile = procoreService.getFilesById(body.getResource_id() + "", body.getProject_id() + "");
-			String filePath = webhookFile.getName_with_path();
-			if (body.getEvent_type().equals("create") || body.getEvent_type().equals("update")) {
-				if (fileRepository.findById(webhookFile.getId()).isPresent()) {
-					Files updated = fileRepository.findById(webhookFile.getId()).get();
-					File f = new File(
-							shareDrive + "/" + updated.getName_with_path());
-					f.delete();
-					FileUtils.downloadUsingStream(webhookFile.getFile_versions().get(0).getUrl(),
-							shareDrive + "/" + filePath);
+		try {
+			if (body.getResource_name().contains("Files")) {
+				Files webhookFile = procoreService.getFilesById(body.getResource_id() + "", body.getProject_id() + "");
+				String filePath = webhookFile.getName_with_path();
+				if (body.getEvent_type().equals("create") || body.getEvent_type().equals("update")) {
+					if (fileRepository.findById(webhookFile.getId()).isPresent()) {
+						Files updated = fileRepository.findById(webhookFile.getId()).get();
+						File f = new File(
+								shareDrive + "/" + updated.getName_with_path());
+						f.delete();
+						FileUtils.downloadUsingStream(webhookFile.getFile_versions().get(0).getUrl(),
+								shareDrive + "/" + filePath);
 
+					} else {
+						java.nio.file.Files
+								.createDirectories(new File(shareDrive + "/" + webhookFile.getName_with_path()
+										.replace(webhookFile.getName(), "")).toPath());
+						FileUtils.downloadUsingStream(webhookFile.getFile_versions().get(0).getUrl(),
+								shareDrive + "/" + filePath);
+
+					}
+					webhookFile.setProject_id(body.getProject_id() + "");
+					fileRepository.save(webhookFile);
 				} else {
-					java.nio.file.Files
-							.createDirectories(new File(shareDrive + "/" + webhookFile.getName_with_path()
-									.replace(webhookFile.getName(), "")).toPath());
-					FileUtils.downloadUsingStream(webhookFile.getFile_versions().get(0).getUrl(),
-							shareDrive + "/" + filePath);
-
+					File f = new File(
+							shareDrive + "/" + webhookFile.getName_with_path());
+					f.delete();
+					fileRepository.delete(webhookFile);
 				}
-				webhookFile.setProject_id(body.getProject_id() + "");
-				fileRepository.save(webhookFile);
-			} else {
-				File f = new File(
-						shareDrive + "/" + webhookFile.getName_with_path());
-				f.delete();
-				fileRepository.delete(webhookFile);
-			}
-		} else if (body.getResource_name().contains("Folders")) {
-			RootFolder webhookfolder = procoreService.getRootFolderById(body.getResource_id() + "",
-					body.getProject_id() + "");
-			String pathFolder = webhookfolder.getName_with_path();
-			if (body.getEvent_type().equals("create")) {
-				java.nio.file.Files.createDirectories(new File(shareDrive + "/" + pathFolder).toPath());
+			} else if (body.getResource_name().contains("Folders")) {
+				RootFolder webhookfolder = procoreService.getRootFolderById(body.getResource_id() + "",
+						body.getProject_id() + "");
+				String pathFolder = webhookfolder.getName_with_path();
+				if (body.getEvent_type().equals("create")) {
+					java.nio.file.Files.createDirectories(new File(shareDrive + "/" + pathFolder).toPath());
 
-			} else if (body.getEvent_type().equals("update")) {
-				if (folderRepository.findById(webhookfolder.getId()).isPresent()) {
-					File updateFolder = new File(shareDrive + "/" + folderRepository
-							.findById(webhookfolder.getId()).get().getName_with_path());
-					updateFolder.renameTo(new File(shareDrive + "/" + pathFolder));
-				}
+				} else if (body.getEvent_type().equals("update")) {
+					if (folderRepository.findById(webhookfolder.getId()).isPresent()) {
+						File updateFolder = new File(shareDrive + "/" + folderRepository
+								.findById(webhookfolder.getId()).get().getName_with_path());
+						updateFolder.renameTo(new File(shareDrive + "/" + pathFolder));
+					}
 
-				webhookfolder.setProject_id(body.getProject_id() + "");
-				folderRepository.save(webhookfolder);
-			} else {
-				if (folderRepository.findById(webhookfolder.getId()).isPresent()) {
-					File updateFolder = new File(shareDrive + "/" + folderRepository
-							.findById(webhookfolder.getId()).get().getName_with_path());
-					updateFolder.delete();
-					folderRepository.delete(folderRepository.findById(webhookfolder.getId()).get());
+					webhookfolder.setProject_id(body.getProject_id() + "");
+					folderRepository.save(webhookfolder);
+				} else {
+					if (folderRepository.findById(webhookfolder.getId()).isPresent()) {
+						File updateFolder = new File(shareDrive + "/" + folderRepository
+								.findById(webhookfolder.getId()).get().getName_with_path());
+						updateFolder.delete();
+						folderRepository.delete(folderRepository.findById(webhookfolder.getId()).get());
+					}
 				}
 			}
+		}catch (Exception e) {
+		e.printStackTrace();
 		}
+	
 	}
 }
