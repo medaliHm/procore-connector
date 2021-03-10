4e154c0111ac274b43d96a1ba32bb5f1925036e7
medaliHm <ehm.medali@gmail.com>
2021-03-10 20:01:06 +0100

    Ad

--- src/main/java/com/procore/connector/TestController.java
+++ src/main/java/com/procore/connector/TestController.java
@@ -38,29 +38,19 @@ public class TestController {
 	
 	public  void runPower() throws IOException {
 		
-		  //Getting the version
-		  String command = "powershell.exe  $connectTestResult = Test-NetConnection -ComputerName csb100320011d345519.file.core.windows.net -Port 445\r\n"
-		  		+ "if ($connectTestResult.TcpTestSucceeded) {cmd.exe /C \"cmdkey /add:`\"csb100320011d345519.file.core.windows.net`\" /user:`\"Azure\\csb100320011d345519`\" /pass:`\"TsVDIQkkGAPxH0e0wdEQx8OSu3TWv56xvJwLX8F/2RuvO2F/JDnnL9XnA36GLD9PzKZ3zgMXWDTQUXwztkgUtQ==`\"\" New-PSDrive -Name Z -PSProvider FileSystem -Root \"\\\\csb100320011d345519.file.core.windows.net\\share-procore\" -Persist } else { Write-Error -Message \"Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port.\"}";
-		  // Executing the command
-		  Process powerShellProcess = Runtime.getRuntime().exec(command);
-		  // Getting the results
-		  powerShellProcess.getOutputStream().close();
-		  String line;
-		  System.err.println("Standard Output:");
-		  BufferedReader stdout = new BufferedReader(new InputStreamReader(
-		    powerShellProcess.getInputStream()));
-		  while ((line = stdout.readLine()) != null) {
-		   System.err.println(line);
-		  }
-		  stdout.close();
-		  System.err.println("Standard Error:");
-		  BufferedReader stderr = new BufferedReader(new InputStreamReader(
-		    powerShellProcess.getErrorStream()));
-		  while ((line = stderr.readLine()) != null) {
-		   System.err.println(line);
-		  }
-		  stderr.close();
-		  System.err.println("Done");
+		 ProcessBuilder builder = new ProcessBuilder(
+		            "cmd.exe", "/c", "cmdkey /add:csb100320011d345519.file.core.windows.net /user:Azure\\\\csb100320011d345519 /pass:TsVDIQkkGAPxH0e0wdEQx8OSu3TWv56xvJwLX8F/2RuvO2F/JDnnL9XnA36GLD9PzKZ3zgMXWDTQUXwztkgUtQ==",
+		            "net use Z: \\\\csb100320011d345519.file.core.windows.net\\share-procore /persistent:Yes\r\n");
+		        builder.redirectErrorStream(true);
+		        Process p = builder.start();
+		        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		        String line;
+		        while (true) {
+		            line = r.readLine();
+		            if (line == null) { break; }
+		            System.err.println(line);
+		        }
+		       
 
 	}
 	@PostMapping("/try")
--- src/main/resources/mount.ps1
+++
@@ -1,9 +0,0 @@
-$connectTestResult = Test-NetConnection -ComputerName csb100320011d345519.file.core.windows.net -Port 445
-if ($connectTestResult.TcpTestSucceeded) {
-    # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"csb100320011d345519.file.core.windows.net`" /user:`"Azure\csb100320011d345519`" /pass:`"TsVDIQkkGAPxH0e0wdEQx8OSu3TWv56xvJwLX8F/2RuvO2F/JDnnL9XnA36GLD9PzKZ3zgMXWDTQUXwztkgUtQ==`""
-    # Mount the drive
-    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\csb100320011d345519.file.core.windows.net\share-procore" -Persist
-} else {
-    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-}
