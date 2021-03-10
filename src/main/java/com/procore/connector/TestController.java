e0ac3dabcea281e5d947e5778d780ba774143ae5
medaliHm <ehm.medali@gmail.com>
2021-03-10 19:25:43 +0100

    Adsds

--- src/main/java/com/procore/connector/TestController.java
+++ src/main/java/com/procore/connector/TestController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,8 +39,8 @@ public class TestController {
 	public  void runPower() throws IOException {
 		
 		  //Getting the version
-		  String command = "powershell.exe  \""+ResourceUtils.getFile(
-			      "classpath:mount.ps").getAbsolutePath()+"\"";
+		  String command = "powershell.exe  $connectTestResult = Test-NetConnection -ComputerName csb100320011d345519.file.core.windows.net -Port 445\r\n"
+		  		+ "if ($connectTestResult.TcpTestSucceeded) {cmd.exe /C \"cmdkey /add:`\"csb100320011d345519.file.core.windows.net`\" /user:`\"Azure\\csb100320011d345519`\" /pass:`\"TsVDIQkkGAPxH0e0wdEQx8OSu3TWv56xvJwLX8F/2RuvO2F/JDnnL9XnA36GLD9PzKZ3zgMXWDTQUXwztkgUtQ==`\"\" New-PSDrive -Name Z -PSProvider FileSystem -Root \"\\\\csb100320011d345519.file.core.windows.net\\share-procore\" -Persist } else { Write-Error -Message \"Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port.\"}";
 		  // Executing the command
 		  Process powerShellProcess = Runtime.getRuntime().exec(command);
 		  // Getting the results
